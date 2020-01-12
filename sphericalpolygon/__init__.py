bd2d0c6ab1df243d7e69c377eb8f8ea2fb014218
Chunxiao Li <lcx@ynao.ac.cn>
2020-01-12 22:07:29 +0800

    Update __init__.py

--- sphericalpolygon/__init__.py
+++ sphericalpolygon/__init__.py
@@ -2,7 +2,7 @@
 sphericalpolygon package
 
 This package is an archive of scientific routines for spherical polygons.   
-Currently, operations on spherical polygons include calculating area and moment of inertia tensor, and determining whether one or more points are inside the spherical polygon.
+Currently, operations on spherical polygons include calculating area or mass(if the area density is given), geometrical or physical moment of inertia tensor, and determining whether one or more points are inside the spherical polygon.
 '''
 
 from .create_polygon import create_polygon
